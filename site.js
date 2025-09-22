f2cb249d47edf5fe58084f5cf26dd25c0f77d689
Jack Behnke <jackbehnke91@gmail.com>
2025-09-22 14:28:25 -0500

    first commit

---
+++ site.js
@@ -0,0 +1,17 @@
+const hours = new Date().getHours() 
+
+const isMorning = hours >= 4 && hours < 12 
+const isAfternoon = hours >= 12 && hours < 17 
+const isEvening = hours >= 17 || hours < 4 
+
+const div = document.querySelector('#welcome')
+
+if(isMorning){
+    div.textContent = "Good Morning"
+}
+if(isAfternoon){
+    div.textContent = "Good Afternoon"
+}
+if(isEvening){
+    div.textContent = "Good Evening"
+}
